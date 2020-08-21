5b3a168cfa39230783b2a8a2a0e05889ea2fa79b
vmisiko <vitormisiko.vm@gmail.com>
2020-08-21 04:27:02 +0300

    corrected on dob label and help text

--- Home/forms.py
+++ Home/forms.py
@@ -6,15 +6,9 @@ from django.contrib.auth.decorators import login_required
 
 class ModelDetailsForms(ModelForm):
 
-    dob = DateField(input_formats=settings.DATE_INPUT_FORMATS)
+    dob = DateField(input_formats=settings.DATE_INPUT_FORMATS,label="Date of Birth (DD/MM/YYYY)", help_text="Sample Input: 15/06/1996")
 
     class Meta:
         model = FormModel
         exclude= ["user"]
-        help_texts = {
-            'dob': 'Sample Input: 27/08/1977',
-        }
-
-        labels = {
-            "dob":"Date of Birth (DD/MM/YYYY)",
-        }
+        
